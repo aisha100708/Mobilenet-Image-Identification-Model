9b8414e28e29ff56994b0d776700e0c7e3869a4d
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-06-05 18:32:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ Webcam.set({
 });
 camera = document.getElementById("camera");
 Webcam.attach(camera);
+function capture() {
+    Webcam.snap(function(data_url)  {
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='"+data_url+"'>";
+    });
+}
+classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+function modelLoaded() {
+    console.log("model loaded");
+}
+function identify() {
+    new_img = document.getElementById("captured_img");
+    classifier.classify(new_img, getResult)
+}
+function getResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
